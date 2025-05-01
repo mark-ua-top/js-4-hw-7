df1d816e4a7c24231e0c24911f92fbbc408e42cd
mark-ua-top <mark.d.nahorniak@gmail.com>
2025-05-01 11:36:18 +0300

    y

---
+++ dist/js-4-hw-7.76ac7884.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=js-4-hw-7.76ac7884.js.map
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import products from "./js/data.js";
+// import createList from "./templaties/smart.hbs";
+
+// const body = document.querySelector("body");
+// console.log(createList(products));
+
+// body.innerHTML = createList(products);
